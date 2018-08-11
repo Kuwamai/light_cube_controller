8ae956783971d57b664c4c294ed8052a3e9dea87
Kuwamai <mitsumex333@gmail.com>
2018-08-11 16:38:33 +0900

    四角形が黒から青に遷移

--- draw.js
+++ draw.js
@@ -3,10 +3,10 @@ var ctx = can.getContext("2d");
 
 var count = 0;//アニメーションカウンター
 var timer = setInterval(function(){
-  ctx.fillStyle="#fff";//消去時の色
-  ctx.clearRect(0,0,300,300);//消す
-  ctx.fillStyle="#f00";//塗りつぶし色を赤に
-  ctx.fillRect(30+count,30+count,30,30);
+  ctx.fillStyle="rgb(255, 255, 255)"//消去時の色
+  ctx.clearRect(0,0,300,300);//消去
+  ctx.fillStyle="rgb(" + 0 + "," + 0 + "," + count + ")"//塗りつぶし色
+  ctx.fillRect(30,30,30,30);
   count++;
   if(count>200){
     clearInterval(timer);
