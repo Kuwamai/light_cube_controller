89abfb199a1a374e0e98e7a14114341ba8bd9e2b
Kuwamai <mitsumex333@gmail.com>
2018-12-14 19:28:59 +0900

    27*27の円を表示

--- draw.js
+++ draw.js
@@ -5,16 +5,31 @@ var ctx = can.getContext("2d");
 can.width = window.innerWidth;
 can.height = window.innerHeight;
 
-var count = 0;//アニメーションカウンター
+//アニメーションカウンター
+var count = 0;
+//円の半径
+var radius = window.innerWidth / (27 * 2 - 1) / 2;
+//円のピッチ
+var pitch = radius * 4;
+
 var timer = setInterval(function(){
-  ctx.fillStyle="rgb(255, 255, 255)"//消去時の色
-  ctx.clearRect(0,0,window.innerWidth,window.innerHeight);//消去
-  ctx.fillStyle="rgb(" + 0 + "," + 0 + "," + count + ")"//塗りつぶし色
-  //ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
-  ctx.arc(30, 30, 30, 0, Math.PI*2);
-  ctx.fill();
-  count++;
-  if(count>249){
-    clearInterval(timer);
-  }
-},20);
+	ctx.fillStyle="rgb(255, 255, 255)"//消去時の色
+	ctx.clearRect(0,0,window.innerWidth,window.innerHeight);//消去
+	ctx.fillStyle="rgb("+count*5+","+count*5+","+count*5+")"//塗りつぶし色
+	
+	for(var i = 0; i < 27; i++){
+		for(var j = 0; j < 27; j++){
+			//円の描画
+			ctx.beginPath();
+			ctx.arc(radius + i * pitch, radius + j * pitch, radius, 0, Math.PI*2);
+			ctx.closePath();
+			//塗りつぶし
+			ctx.fill();
+		}
+	}
+
+	count++;
+	if(count>50){
+		clearInterval(timer);
+	}
+},250);
