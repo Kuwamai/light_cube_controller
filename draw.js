a58c60cebe1099bf72e6904226f24c350535f106
Kuwamai <mitsumex333@gmail.com>
2018-12-14 16:55:47 +0900

    画面サイズに合わせてCanvasサイズも変更

--- draw.js
+++ draw.js
@@ -2,15 +2,15 @@ var can = document.getElementById("canvas");
 var ctx = can.getContext("2d");
 
 //canvasサイズの変更
-can.width = 400;
-can.height = 400;
+can.width = window.innerWidth;
+can.height = window.innerHeight;
 
 var count = 0;//アニメーションカウンター
 var timer = setInterval(function(){
   ctx.fillStyle="rgb(255, 255, 255)"//消去時の色
   ctx.clearRect(0,0,300,300);//消去
   ctx.fillStyle="rgb(" + 0 + "," + 0 + "," + count + ")"//塗りつぶし色
-  //ctx.fillRect(30,30,30,30);
+  //ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
   ctx.arc(30, 30, 30, 0, Math.PI*2);
   ctx.fill();
   count++;
