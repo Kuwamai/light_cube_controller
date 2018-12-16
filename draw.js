fd04b9c4e3cbd43af8664bda0c869a5e8387b9b8
Kuwamai <mitsumex333@gmail.com>
2018-12-16 17:59:15 +0900

    円の描画順序変更

--- draw.js
+++ draw.js
@@ -17,14 +17,19 @@ var timer = setInterval(function(){
 	ctx.clearRect(0,0,window.innerWidth,window.innerHeight);//消去
 	ctx.fillStyle="rgb("+count*5+","+count*5+","+count*5+")"//塗りつぶし色
 	
-	for(var i = 0; i < 27; i++){
-		for(var j = 0; j < 27; j++){
-			//円の描画
-			ctx.beginPath();
-			ctx.arc(radius + i * pitch, radius + j * pitch, radius, 0, Math.PI*2);
-			ctx.closePath();
-			//塗りつぶし
-			ctx.fill();
+	for(var i = 0; i < 9; i++){
+		for(var j = 0; j < 9; j++){
+			for(var k = 0; k < 3; k++){
+				for(var l = 0; l < 3; l++){
+					//円の描画
+					ctx.beginPath();
+					ctx.arc(radius + l*pitch + 3*j*pitch, radius + k*pitch + 3*i*pitch, radius, 0, Math.PI*2);
+
+					ctx.closePath();
+					//塗りつぶし
+					ctx.fill();
+				}
+			}
 		}
 	}
 
